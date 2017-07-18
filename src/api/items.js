e5a5fcfccba1d6bd242b33ca5866162e9150b158
loren.grixti <lorengrx@gmail.com>
2017-07-18 14:44:48 +0200

    updating main node entry point

--- src/api/items.js
+++ src/api/items.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default class ApiItems {
     static getList(limit) {
-        return axios.get("http://localhost:3001/api/items?limit=" + limit);
+        return axios.get("api/items?limit=" + limit);
     }
 }
